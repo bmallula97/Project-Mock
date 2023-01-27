67e787b5ad16d65e282e6310892c37fa446cbe15
reactvehiclerentalsystem <reactvehiclerentalsystem@gmail.com>
2023-01-27 11:06:12 +0530

    Create RemoveElements.java

---
+++ RemoveElements.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class RemoveElements {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(36);
+        queue.add(45);
+        queue.add(21);
+        System.out.println("After removing the element");
+      queue.remove(45);
+        Iterator it = queue.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+    
+}
