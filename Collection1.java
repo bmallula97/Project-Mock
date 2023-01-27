12bf3546c4c5d8e75d7c6bc1cb586ea58e889271
reactvehiclerentalsystem <reactvehiclerentalsystem@gmail.com>
2023-01-27 10:33:11 +0530

    New begining

---
+++ Collection1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Collection1 {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(78);
+        queue.add(21);
+        queue.add(45);
+        System.out.println(queue.peek());
+        System.out.println("Adding elements to the queue");
+        Iterator it = queue.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        
+    }
+}
